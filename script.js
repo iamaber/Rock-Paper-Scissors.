d72ed42c01a1473b6fab7890bd4bcb3fb65dbe78
Aber Islam <71089269+iamaber@users.noreply.github.com>
2023-10-31 09:16:38 +0600

    Added All the functions
    added playRound and game

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+let playerScore = 0
+let computerScore = 0
 
 function getComputerChoice() {
     var things = ['Rock', 'Paper', 'Scissors'];
     var choice = things[Math.floor(Math.random() * things.length)];
     return choice;
 }
-
 
 function playRound(playerSelection, computerSelection) {
     let roundWinner;
@@ -16,22 +17,46 @@ function playRound(playerSelection, computerSelection) {
         (playerSelection === 'Scissors' && computerSelection === 'Paper') ||
         (playerSelection === 'Paper' && computerSelection === 'Rock')
     ) {
-        roundWinner = 'You Win! ' + playerSelection + ' beats ' + computerSelection
+        playerScore++
+
+        roundWinner = 'You Win! ' + playerSelection + ' beats ' + computerSelection;
+
+
+
     }
     if (
         (computerSelection === 'Rock' && playerSelection === 'Scissors') ||
         (computerSelection === 'Scissors' && playerSelection === 'Paper') ||
         (computerSelection === 'Paper' && playerSelection === 'Rock')
     ) {
+        computerScore++
 
-        roundWinner = 'You Lose! ' + computerSelection + ' beats ' + playerSelection
+        roundWinner = 'You Lose! ' + computerSelection + ' beats ' + playerSelection;
+
+
+
     }
+
     return roundWinner;
 }
 
 
+function game() {
+    let message;
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = window.prompt();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+    if (playerScore > computerScore) {
+        message = 'YOU ARE THE WINNER';
+    }
+    else {
+        message = 'YOU ARE THE LOOSER';
+    }
+    return message;
+
+}
 
 
-let playerSelection = 'Rock';
 const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+console.log(game());
